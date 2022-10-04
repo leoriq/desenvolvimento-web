74b3e4b112ba37cb6254865506490257532cf321
leonardo.salandim <leonardo.salandim@digitalsys.com.br>
2022-10-04 00:35:24 -0300

    fix File

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 import Sequelize from "sequelize";
 import databaseConfig from "../config/database";
+
 import User from "../app/models/User";
 import File from "../app/models/File";
 import Ad from "../app/models/Ad";
